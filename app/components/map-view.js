ed247688332d423b55d01bc9afc9895d0ee85a06
holaliamewu <me.amewu@gmail.com>
2025-04-09 15:09:14 +0000

    fix: change mapbox api

--- app/components/map-view.js
+++ app/components/map-view.js
@@ -5,7 +5,8 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 export default function MapView({currentLocation}) {
 
-  const mapBoxApiKey = process.env.MAPBOX_API_KEY
+  const mapBoxApiKey = process.env.NEXT_PUBLIC_MAPBOX_API_KEY;
+  
   return (
     <Map
       mapboxAccessToken= {mapBoxApiKey}
