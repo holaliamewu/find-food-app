9ede1ba67aa361f3fa9af6134fac54591acda7ff
holaliamewu <me.amewu@gmail.com>
2025-04-17 11:30:23 +0000

    Update page.js

--- app/map/page.js
+++ app/map/page.js
@@ -12,7 +12,7 @@ export default function MainPage() {
     <div className="font-[manrope]" >
       <MapView />
       <section className="w-full py-2 fixed bottom-6 left-0 z-1000" >
-        <div className="flex flex-col bg-white px-4 py-2 w-9/10 md:w-fit max-w-[350px] h-fit mx-auto relative transition-all border border-stone-200 shadow gap-3 rounded-[12px] font-bold text-center" >
+        <div className="flex flex-col bg-white px-4 py-2 w-fit md:w-fit max-w-[350px] h-fit mx-auto relative transition-all border border-stone-200 shadow gap-3 rounded-[12px] font-bold text-center" >
                 <h1 onClick={ () => setShowField(true)} className="flex items-center justify-between text-left  font-[800] cursor-pointer" >Add your fav Spot 
               { showField && 
               <svg className="absolute right-4 top-3" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 18 18" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>}
