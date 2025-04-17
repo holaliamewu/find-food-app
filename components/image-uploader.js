797369c5b65fabd4d6d396d13cfdb7f01ea0528d
holaliamewu <me.amewu@gmail.com>
2025-04-17 11:21:53 +0000

    fix: input width

--- components/image-uploader.js
+++ components/image-uploader.js
@@ -46,7 +46,7 @@ export default function ImageUploader() {
         className="hidden bg-blue-50 border-dashed px-3 py-1.5 border-2 border-stone-200 rounded-[8px] cursor-pointer"
       />
       <span
-        onClick={ () => { imageCollector.click()}}
+        onClick={ () => { imageCollector?.click()}}
         className="flex items-center justify-center bg-blue-50 border-dashed px-3 py-1.5 border border-stone-200 rounded-[8px] cursor-pointer"
       >
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-image-icon lucide-image"><rect width="18" height="18" x="3" y="3" rx="2" ry="2"/><circle cx="9" cy="9" r="2"/><path d="m21 15-3.086-3.086a2 2 0 0 0-2.828 0L6 21"/></svg>
